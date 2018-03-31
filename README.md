918a0d897ed57196505dac9d67a1d3e9e34c7b57
qiaoshuai0914 <qiaoshuai0914@163.com>
2018-03-31 17:57:55 +0800

    二叉树模块

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# datastructuresalgorithms
+# datas tructures algorithms
 数据结构算法实例代码
 主要用于自己积累数据结构和算法
 （1）binarytree  二叉树模块，主要是二叉树结构的相关操作。
--- binarytree/src/main/java/com/qs/algorithms/binarytree/BinaryTree.java
+++ binarytree/src/main/java/com/qs/algorithms/binarytree/BinaryTree.java
@@ -1,28 +1,49 @@
 package com.qs.algorithms.binarytree;
 
+import java.util.LinkedList;
 import java.util.Stack;
 
-public class BinaryTree {
+/**
+ * 二叉树基础操作总结
+ * @author qiaoshuai
+ */
+public class BinaryTree<T> {
 
+    /*
+     * 先序创建二叉树
+     * 返回：根节点
+     */
+    public Node<T>  creatBinaryPre(LinkedList<T> treeData)
+    {
+        Node<T> root=null;
+        T data=treeData.removeFirst();
+        if (data!=null)
+        {
+            root=new Node<T>();
+            root.setLlfte(creatBinaryPre(treeData));
+            root.setRight(creatBinaryPre(treeData));
+        }
+        return root;
+    }
     /***
-     * 前序遍历   根节点    左子树   右子树
+     * 前序遍历   根节点--左子树--右子树
      * 递归版
      */
-    public void  preOrder(Node  root){
+    public void  preRecurTraverse(Node  root){
         if(root==null){
             System.out.println("你是不是有病，null你调用我的方法干嘛");
             return;
         }
-        System.out.println(root.getData().byteValue());
-        preOrder(root.getLlfte());
-        preOrder(root.getRight());
+        System.out.println(root.getData());
+        preRecurTraverse(root.getLlfte());
+        preRecurTraverse(root.getRight());
     }
 
     /**
      * 非递归遍历   大概遍历根节点，然后把左节点一直放入到栈的数据结构。然后把栈中的元素的取出来遍历柚子树。
      * @param root
      */
-    public   void  preOrder2(Node root){
+    public   void  preRecurTraverse2(Node root){
 
         Stack<Node> stack  =new Stack<Node>();
         while (root!=null||!stack.empty()){
@@ -40,22 +61,24 @@ public class BinaryTree {
     }
 
     /**
-     * 递归中序遍历  左子树    根节点    右子树
+     * 递归中序遍历  左子树--根节点--右子树
      *
      */
-    public  void    inorderTraverse(Node root){
-
+    public  void    midRecurTraverse(Node root){
         if(root==null){
             System.out.println("你是不是有病，null你调用我的方法干嘛");
             return;
         }
-        inorderTraverse(root.getLlfte());
-        System.out.println(root.getData().byteValue());
-        inorderTraverse(root.getRight());
+        midRecurTraverse(root.getLlfte());
+        System.out.println(root.getData());
+        midRecurTraverse(root.getRight());
 
     }
-    //非递归算法的中序遍历
-    public void inorderTraverse2(Node root){
+    /**
+     * 非递归算法的中序遍历
+     * @param root
+     */
+    public void midRecurTraverse2(Node root){
         System.out.println("中根次序遍历（非递归）：");
         Stack<Node> stack = new Stack<Node>();//创建空栈
 
@@ -73,21 +96,21 @@ public class BinaryTree {
         System.out.println("^");
     }
     /**
-     * 递归后序遍历  左子树       右子树  根节点
+     * 递归后序遍历  左子树---右子树---根节点
      *
      */
-    public  void    postorder(Node root){
+    public  void    bacRecurTraverse(Node root){
 
         if(root==null){
             System.out.println("你是不是有病，null你调用我的方法干嘛");
             return;
         }
-        postorder(root.getLlfte());
-        postorder(root.getRight());
-        System.out.println(root.getData().byteValue());
+        bacRecurTraverse(root.getLlfte());
+        bacRecurTraverse(root.getRight());
+        System.out.println(root.getData());
 
     }
-    public void postorderTrave(Node  root){
+    public void bacRecurTraverse2(Node  root){
         System.out.println("后根次序遍历（非递归）：");
         Stack<Node> stack = new Stack<Node>();
         //待操作栈
@@ -116,15 +139,15 @@ public class BinaryTree {
         System.out.println();
     }
 
-    public  class  Node{
-        private  Integer  data;
+    public  class  Node<T>{
+        private  T  data;
         private   Node    llfte;
         private   Node     right;
-        public Integer getData() {
+        public T getData() {
             return data;
         }
 
-        public void setData(Integer data) {
+        public void setData(T data) {
             this.data = data;
         }
 
